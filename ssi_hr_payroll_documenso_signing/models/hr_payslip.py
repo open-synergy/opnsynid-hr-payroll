4c9b9e7ea50f09be6ce9086d3a39495986de5990
Andhitia Rama <andhitia.r@gmail.com>
2026-03-20 21:14:29 +0700

    [ADD] ssi_hr_payroll_documenso_signing

---
+++ ssi_hr_payroll_documenso_signing/models/hr_payslip.py
@@ -0,0 +1,15 @@
+# Copyright 2025 OpenSynergy Indonesia
+# Copyright 2025 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+
+from odoo import models
+
+
+class HrPayslip(models.Model):  # pylint: disable=R0903
+    _name = "hr.payslip"
+    _inherit = [
+        "hr.payslip",
+        "mixin.documenso_signing",
+    ]
+
+    _documenso_signing_create_page = True
