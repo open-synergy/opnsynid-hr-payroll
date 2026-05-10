a7ee000fada349b3687246cc9d7600b2df0cfb37
Andhitia Rama <andhitia.r@gmail.com>
2026-05-10 18:05:05 +0700

    [UPD] ssi_hr_payroll_batch_operating_unit
    * Tambahkan hr_payslip.py: Python constrains untuk memastikan operating_unit_id payslip sama dengan batch
    * Pisahkan kondisi ke method _check_payslip_batch_operating_unit_condition (return bool, overridable)
    * Pesan error dan raise ValidationError tetap di _check_batch_operating_unit

--- ssi_hr_payroll_batch_operating_unit/models/__init__.py
+++ ssi_hr_payroll_batch_operating_unit/models/__init__.py
@@ -2,4 +2,5 @@
 # Copyright 2026 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from . import hr_payslip  # noqa: F401
 from . import hr_payslip_batch  # noqa: F401
