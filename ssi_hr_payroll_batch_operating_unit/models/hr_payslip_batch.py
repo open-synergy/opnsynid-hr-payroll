a717dc0f0183dc90a57b21f9df536d154b8b40b0
Andhitia Rama <andhitia.r@gmail.com>
2026-05-10 17:26:36 +0700

    [UPD] ssi_hr_payroll_batch_operating_unit
    * Propagate operating_unit_id from batch to generated payslips
    * Add ssi_hr_payroll_operating_unit as dependency

--- ssi_hr_payroll_batch_operating_unit/models/hr_payslip_batch.py
+++ ssi_hr_payroll_batch_operating_unit/models/hr_payslip_batch.py
@@ -10,6 +10,8 @@ class HrPayslipBatch(models.Model):  # pylint: disable=too-few-public-methods
     Extends hr.payslip_batch with operating unit support.
     Adds mixin.single_operating_unit so payslip batch documents
     can be scoped to a specific operating unit.
+    Overrides _prepare_payslip_data to propagate operating_unit_id
+    to each generated payslip.
     """
 
     _name = "hr.payslip_batch"
@@ -17,3 +19,8 @@ class HrPayslipBatch(models.Model):  # pylint: disable=too-few-public-methods
         "hr.payslip_batch",
         "mixin.single_operating_unit",
     ]
+
+    def _prepare_payslip_data(self, employee):
+        res = super()._prepare_payslip_data(employee)
+        res["operating_unit_id"] = self.operating_unit_id.id
+        return res
