e861d3256b8ad4abdfe4030894b126cb6e27cc0b
Andhitia Rama <andhitia.r@gmail.com>
2026-06-11 06:08:27 +0700

    [UPD] ssi_hr_payroll_operating_unit
    * Kuatkan assertion unittest: ganti conditional if menjadi assertTrue wajib
      agar test gagal eksplisit jika payslip atau move_id tidak ada di registry

--- ssi_hr_payroll_operating_unit/tests/test_hr_payslip_operating_unit.py
+++ ssi_hr_payroll_operating_unit/tests/test_hr_payslip_operating_unit.py
@@ -12,9 +12,10 @@ class TestHrPayslipOperatingUnit(YamlTransactionCase):
     def test_hr_payslip_operating_unit(self):
         self.run_yaml_scenario("test_data_hr_payslip_operating_unit.yaml")
         payslip = self.registry.get("payslip")
-        if payslip and payslip.move_id:
-            self.assertEqual(
-                payslip.move_id.operating_unit_id,
-                payslip.operating_unit_id,
-                "account.move should have the same operating_unit_id as the payslip",
-            )
+        self.assertTrue(payslip, "Payslip should be in registry after scenario")
+        self.assertTrue(payslip.move_id, "Payslip should have an accounting entry")
+        self.assertEqual(
+            payslip.move_id.operating_unit_id,
+            payslip.operating_unit_id,
+            "account.move should have the same operating_unit_id as the payslip",
+        )
