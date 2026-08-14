2f70ff7619a499ea4a30baa323d241458bd00a9a
Andhitia Rama <andhitia.r@gmail.com>
2026-08-14 10:18:17 +0700

    [UPD] ssi_hr_payroll_batch_summary_report
    * Ganti nama direktori report/ menjadi reports/ sesuai konvensi
      struktur modul SSI (ejaan jamak)
    * Sesuaikan import paket di __init__.py modul
    * Sesuaikan path template pada kunci data di __manifest__.py
    
    Rename dilakukan dengan git mv sehingga riwayat berkas terjaga.
    Tidak ada perubahan perilaku laporan; reports.xml di akar modul
    tidak disentuh.
    
    Closes #203

--- ssi_hr_payroll_batch_summary_report/__init__.py
+++ ssi_hr_payroll_batch_summary_report/__init__.py
@@ -1,4 +1,4 @@
 # Copyright 2024 OpenSynergy Indonesia
 # Copyright 2024 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
-from . import models, report
+from . import models, reports
